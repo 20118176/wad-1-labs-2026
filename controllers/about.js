e84c1e4e5b3174e0cfe4a8593b6b6c17c4da8656
unknown <20118176@setu.ie>
2026-02-28 21:00:54 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,12 +1,20 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import empStore from "../models/emp-store.js";
 
 const about = {
-    createView(request, response) {
-      logger.info("About page loading!");
-      response.send('About the Playlist app');   
-    },
-  };  
+  createView(request, response) {
+    logger.info("About page loading!");
+    
+    const viewData = {
+      title: "About the playlist app.",
+      emps: empStore.getEmpInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('about', viewData);   
+  },
+}
 
 export default about;
