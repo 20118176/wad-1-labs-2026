e9dc55dec3756aa552be3012b9c9703a1b9dd8c3
(20118176) Abdulahi Ahmed <20118176@setu.ie>
2026-03-11 14:10:51 +0000

    Completed Delete Functions Lab

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -25,6 +25,13 @@ const dashboard = {
     playlistStore.addPlaylist(newPlayList);
     response.redirect('/dashboard');
   },
+  deletePlaylist(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Deleting Playlist ${playlistId}`);
+    playlistStore.removePlaylist(playlistId);
+    response.redirect("/dashboard");
+  },
+
 
 };
 
